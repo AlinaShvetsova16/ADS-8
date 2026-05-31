f9cfe11f894e1fe9fbf818d3017b25f9bb549774
AlinaShvetsova16 <alinashvetsova16@gmail.com>
2026-05-31 23:21:02 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
 // Copyright 2021 NNTU-CS
-#include "bst.h"
-#include "src/alg.cpp"
 #include <iostream>
+#include <string>
+#include "bst.h"
 
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
 int main() {
     BST<std::string> tree;
-    makeTree(tree, "src/war_peace.txt");
+    const char* filename = "src/war_peace.txt";
+    makeTree(tree, filename);
     std::cout << "Tree depth: " << tree.depth() << std::endl;
+    std::string searchWord = "war";
+    int count = tree.search(searchWord);
+    if (count > 0) {
+        std::cout << "Word '" << searchWord << "' found " << count << " times." << std::endl;
+    }
     printFreq(tree);
     return 0;
 }
